b232068416273b949368f7b0df8e6abd08873ba6
Aoxiang Zhang <ax706429@dal.ca>
2021-07-15 22:54:36 -0300

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
+
 # build
-yarn run build
+npm run build
+
 # navigate into the build output directory
 cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/Auston-Zhang/panoVue2 master:gh-pages
+git push -f https://github.com/Auston-Zhang/panoVue2.git master:gh-pages
 cd -
